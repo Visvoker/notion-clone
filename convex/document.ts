89386d13beedc3fa8c20b4a926caa4e6cef51420
visvoker <s2089533@gmail.com>
2024-09-11 18:08:20 +0800

    Archive Functionality

--- convex/document.ts
+++ convex/document.ts
@@ -3,6 +3,55 @@ import { v } from "convex/values"
 import { mutation, query } from "./_generated/server"
 import { Doc, Id } from "./_generated/dataModel"
 
+export const archive = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identify = await ctx.auth.getUserIdentity();
+
+    if (!identify) {
+      throw new Error("Not authenticated")
+    }
+
+    const userId = identify.subject;
+
+    const existingDocument = await ctx.db.get(args.id)
+
+    if (!existingDocument) {
+      throw new Error("Not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+    const recursiveArchive = async (documentId: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) => (
+          q
+            .eq("userId", userId)
+            .eq("parentDocument", documentId)
+        ))
+        .collect();
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, {
+          isArchived: true,
+        });
+
+        await recursiveArchive(child._id);
+      }
+    }
+
+    const document = await ctx.db.patch(args.id, {
+      isArchived: true,
+    });
+
+    recursiveArchive(args.id);
+
+    return document;
+  }
+})
+
 export const getSidebar = query({
   args: {
     parentDocument: v.optional(v.id("documents"))
@@ -11,7 +60,7 @@ export const getSidebar = query({
     const identify = await ctx.auth.getUserIdentity();
 
     if (!identify) {
-      throw new Error("NOt authenticated");
+      throw new Error("Not authenticated");
     }
 
     const userId = identify.subject;
