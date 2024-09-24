6f38e7c3a48e2943acd88a95ece2f6af142e5420
visvoker <s2089533@gmail.com>
2024-09-24 13:54:42 +0800

    Advanced file upload

--- components/cover.tsx
+++ components/cover.tsx
@@ -3,6 +3,7 @@
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useCoverImage } from "@/hooks/use-cover-image";
+import { useEdgeStore } from "@/lib/edgestore";
 import { cn } from "@/lib/utils";
 import { useMutation } from "convex/react";
 import { ImageIcon, X } from "lucide-react";
@@ -20,11 +21,17 @@ export const Cover = ({
   url,
   preview,
 }: CoverImageProps) => {
+  const { edgestore } = useEdgeStore();
   const coverImage = useCoverImage();
   const params = useParams();
   const removeCoverImage = useMutation(api.documents.removeCoverImage);
 
-  const onRemove = () => {
+  const onRemove = async () => {
+    if (url) {
+      await edgestore.publicFiles.delete({
+        url: url,
+      })
+    }
     removeCoverImage({
       id: params.documentId as Id<"documents">
     });
@@ -48,7 +55,7 @@ export const Cover = ({
       {url && !preview && (
         <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
           <Button
-            onClick={coverImage.onOpen}
+            onClick={() => coverImage.onReplace(url)}
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
